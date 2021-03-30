21b8b942d7d6d8be06566448e1fb4782fe46645e
PulokSec <riopulok@gmial.com>
2021-03-30 18:37:34 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDBrYEm87lXELoCOH9m0xKaUtmrY4EEGLQ",
+  authDomain: "ema-john-master.firebaseapp.com",
+  projectId: "ema-john-master",
+  storageBucket: "ema-john-master.appspot.com",
+  messagingSenderId: "541907723365",
+  appId: "1:541907723365:web:aec8f00553b2a2660b928c",
+  measurementId: "G-9PH7C9DWVQ"
+};
+  export default firebaseConfig;
